028ffcabe0f1d92b5d77b7a451360c3389bad4e5
TheDoctor-95 <pau.duran.godino.95@gmail.com>
2017-09-29 10:29:31 +0200

    try

--- src/main/java/daw/basket/data/repository/JugadorRepository.java
+++ src/main/java/daw/basket/data/repository/JugadorRepository.java
@@ -10,7 +10,6 @@ import org.springframework.data.jpa.repository.Query;
 import java.time.LocalDate;
 import java.util.List;
 
-
 public interface JugadorRepository extends JpaRepository<Jugador, Long> {
 
 
