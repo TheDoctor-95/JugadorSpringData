2650d6a45a865d3dd92a3793dae0318575d12157
Pau Duran <pau.duran.godino.95@gmail.com>
2017-09-29 11:00:06 +0200

    gchgvhgvhg

--- src/main/java/daw/basket/data/repository/JugadorRepository.java
+++ src/main/java/daw/basket/data/repository/JugadorRepository.java
@@ -11,7 +11,7 @@ import java.time.LocalDate;
 import java.util.List;
 public interface JugadorRepository extends JpaRepository<Jugador, Long> {
 
-    //prueba
+    //prueba dfxdf
 
     List<Jugador> findByNombreContaining(String nombre);
 
